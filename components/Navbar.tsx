81c13410815012cb25e8cd63f2286ddcbee1c4b1
satwik011 <satwiknakhate2000@gmail.com>
2025-01-26 01:48:49 +0530

    complete project deploy

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,10 @@
 
 import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 const Navbar = async () => {
     const session = await auth()
@@ -16,17 +18,26 @@ const Navbar = async () => {
                     {session && session?.user ? (
                         <>
                             <Link href="/startup/create">
-                                <span>Create</span>
+                            <span className="max-sm:hidden">Create</span>
+                            <BadgePlus className="sm:hidden size-6 " />
+
                             </Link>
                             <form action={async () => {
                             'use server'
                             await signOut({redirectTo :'/'})
                         }}>
-                            <button type="submit">logout</button>
+                            <button type="submit">
+                            <span className="max-sm:hidden">Logout</span>
+                            <LogOut className="sm:hidden size-6 text-red-500" />
+
+                            </button>
                         </form>
                           
                             <Link href={`/user/${session?.id}`}>
-                                <span>{session?.user?.name} </span>
+                            <Avatar className="size-10">
+                            <AvatarImage  src={session?.user?.image || ''} alt={session?.user?.name || ''} />
+                            <AvatarFallback>AV</AvatarFallback>
+                            </Avatar>
                             </Link>
                         </>
                     ) : <>
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { author } from './author'
 import { startup } from './startups'
+import { playlist } from './playlist'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [author,startup],
+  types: [author,startup,playlist],
 }
